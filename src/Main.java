79a845cb8b6e8fba9ef56bbd5eb93fe8c3d4e018
PaulinOitavo <henriquent90@gmail.com>
2023-05-04 11:55:03 -0300

    Aula de introdução OO

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // laço while
-        int contador = 0;
-        while (contador < 10){
-            System.out.println("contador " + contador);
-            // incremento
-            contador = contador + 1;
-        }
+        // Aula introdutória de Orientação à Objetos
 
-        //laço for
-        for(int x = 0; x < 10; x++){
-            System.out.println("contador " + x);
-        }
-
-        // laço do
-        int cont = 0;
-        do{
-            //processo
-            System.out.println(cont);
-            cont++; // incremento
-        }while (cont <= 10);
     }
 }
